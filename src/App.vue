c7f8c77bcda053a5555b143965718780c880e503
stappost <emanuele.scarpantoni@gmail.com>
2024-01-12 15:31:42 +0100

    add searchbar and axios

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <script>
+// AXIOS 
+import axios from "axios";
+// COMPONENTS 
+import AppHeader from './components/AppHeader.vue';
+// STORE 
+import { store } from './store'
 export default {
-
+  data() {
+    return {
+      store
+    }
+  },
+  components: {
+    AppHeader,
+  },
+  methods: {
+    load_films() {
+      if (store.search != '') {
+        let apifilms = store.apiUrlFilm
+        apifilms += `?api_key=${store.api_key}&query=${store.search}`
+        axios.get(apifilms).then(results => {
+          store.films = results.data.results
+          console.log(store.films)
+        })
+      }
+    },
+  }
 }
 </script>
 <template lang="">
   <div>
-    
+    <AppHeader @load_page="load_films"/>
   </div>
 </template>
 <style lang="">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+import { store } from '../store'
+export default {
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+<template lang="">
+    <header>
+        <input type="text" v-model='store.search'>
+        <button @click="$emit('load_page')">search</button>
+    </header>
+</template>
+<style lang="">
+    
+</style>
