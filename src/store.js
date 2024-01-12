37683bfc13b4c601f9225eb69043a599824888dd
stappost <emanuele.scarpantoni@gmail.com>
2024-01-12 14:58:00 +0100

    MILESTONE 0 add store.js

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { reactive } from 'vue';
+export const store = reactive({
+    apiUrlFilm: 'https://api.themoviedb.org/3/search/movie',
+    apiUrlSerie: 'https://api.themoviedb.org/3/search/tv',
+    films: [],
+    seria_tv: [],
+    api_key: '1be1847942544cb9ad6772227c9a0766',
+    search: '',
+    loading: true
+})
